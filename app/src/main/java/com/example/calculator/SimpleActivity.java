060701e408a83e3cfa9bc785b5d9e6c919feecea
Kamil Jurek <informator017@wp.pl>
2019-03-10 12:37:23 +0100

    Fix TextView in simple calculations.

--- app/src/main/java/com/example/calculator/SimpleActivity.java
+++ app/src/main/java/com/example/calculator/SimpleActivity.java
@@ -2,12 +2,17 @@ package com.example.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SimpleActivity extends AppCompatActivity {
+
+    TextView resultView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple);
+        resultView = (TextView) findViewById(R.id.result_view);
+        resultView.setKeyListener(null);
     }
 }
