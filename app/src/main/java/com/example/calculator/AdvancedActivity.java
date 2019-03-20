aa5391afaf705a8739132f217101444edfd589d2
Kamil Jurek <informator017@wp.pl>
2019-03-20 14:45:07 +0100

    Add Advanced Calculator portrait layout.

---
+++ app/src/main/java/com/example/calculator/AdvancedActivity.java
@@ -0,0 +1,13 @@
+package com.example.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdvancedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advanced);
+    }
+}
